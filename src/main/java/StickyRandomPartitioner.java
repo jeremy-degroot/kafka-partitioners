0a16fbd8aee63d7b02e3a4d430d003cb05f7cc57
Jeremy <jeremy.degroot@distilnetworks.com>
2019-03-30 15:04:24 -0400

    WIP, needs testing

--- src/main/java/StickyRandomPartitioner.java
+++ src/main/java/StickyRandomPartitioner.java
@@ -2,6 +2,7 @@ import com.google.common.base.Preconditions;
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -12,10 +13,25 @@ import java.util.Random;
 public class StickyRandomPartitioner implements Partitioner {
   Long stickTime;
   TemporalUnit timeUnit;
+  Clock clock;
 
-  public StickyRandomPartitioner(Long stickTime, TemporalUnit timeUnit) {
+  /**
+   * Constructor to be used with a call to configure()
+   */
+  public  StickyRandomPartitioner() {
+    this.clock = Clock.systemUTC();
+  }
+
+  /**
+   * Constructor for testing
+   * @param stickTime
+   * @param timeUnit
+   * @param clock
+   */
+  public StickyRandomPartitioner(Long stickTime, TemporalUnit timeUnit, Clock clock) {
     setStickTime(stickTime);
     setTimeUnit(timeUnit);
+    this.clock = clock;
   }
 
   public void setStickTime(Long stickTime) {
@@ -27,14 +43,15 @@ public class StickyRandomPartitioner implements Partitioner {
   }
 
   /**
+   * TODO Performance could be improved by updating the current time in a thread instead of on demand
    * @param topic
    * @param key
    * @param cluster
    * @return
    */
   public int partition(String topic, Long key, Cluster cluster) {
-    LocalDateTime now = LocalDateTime.now(ZoneId.systemDefault()).truncatedTo(timeUnit);
-    long seconds = now.toEpochSecond(ZoneOffset.of(ZoneId.systemDefault().getId()));
+    LocalDateTime now = LocalDateTime.now(clock);
+    long seconds = now.toEpochSecond(ZoneOffset.of(clock.getZone().getId()));
     long stickDuration = timeUnit.getDuration().getSeconds() * stickTime;
     long partitionKey = Math.floorDiv(seconds, stickDuration) + key;
 
